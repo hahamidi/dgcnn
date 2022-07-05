527a42dc57a33fb05ccaf3e423d660dc48f0bf1e
hasanhamidi <hamidi3031@gmail.com>
2022-07-05 17:36:51 +0430

    change test bach size to 64

--- main_partseg.py
+++ main_partseg.py
@@ -373,7 +373,7 @@ if __name__ == "__main__":
                                  'motor', 'mug', 'pistol', 'rocket', 'skateboard', 'table'])
     parser.add_argument('--batch_size', type=int, default=16, metavar='batch_size',
                         help='Size of batch)')
-    parser.add_argument('--test_batch_size', type=int, default=16, metavar='batch_size',
+    parser.add_argument('--test_batch_size', type=int, default=64, metavar='batch_size',
                         help='Size of batch)')
     parser.add_argument('--epochs', type=int, default=200, metavar='N',
                         help='number of episode to train ')
