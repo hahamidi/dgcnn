20c513c5354b5be6171e1f6732ee370044c7da7d
hasanhamidi <hamidi3031@gmail.com>
2022-07-17 16:32:50 +0430

    1

--- pointNetP.py
+++ pointNetP.py
@@ -75,6 +75,8 @@ class PointNet2SemSegSSG(nn.Module):
     def _break_up_pc(self, pc):
         xyz = pc[..., 0:3].contiguous()
         features = pc[..., 3:].transpose(1, 2).contiguous() if pc.size(-1) > 3 else None
+        print(xyz)
+        print(features)
 
         return xyz, features
         
