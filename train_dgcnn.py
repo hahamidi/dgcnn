5da5f340fa79d1ea1590d80bd917ac7190fd9769
hasanhamidi <hamidi3031@gmail.com>
2022-07-17 21:03:51 +0430

    1

--- train_dgcnn.py
+++ train_dgcnn.py
@@ -290,6 +290,7 @@ if __name__ == '__main__':
 
     optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
     model.to(device)
     print(device)
 
