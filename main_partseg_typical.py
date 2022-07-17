49a51e189aae6fb13047f9810ca5a0b61138baa0
hasanhamidi <hamidi3031@gmail.com>
2022-07-17 12:55:16 +0430

    1

--- main_partseg_typical.py
+++ main_partseg_typical.py
@@ -16,7 +16,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim.lr_scheduler import CosineAnnealingLR, StepLR
-from part import ShapeNetPart
+from data import ShapeNetPart
 from model import DGCNN_partseg
 import numpy as np
 from torch.utils.data import DataLoader
