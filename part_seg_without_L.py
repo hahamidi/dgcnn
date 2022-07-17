d3c901fd963c5cbb624cfd8e1c794ac410f9d6dc
hasanhamidi <hamidi3031@gmail.com>
2022-07-17 13:56:15 +0430

    1

--- part_seg_without_L.py
+++ part_seg_without_L.py
@@ -117,6 +117,7 @@ class Trainer():
                         # print(targets)
     
                         points, targets = points.to(self.device), targets.to(self.device)
+                        data = points.permute(0, 2, 1)
 
                         if points.shape[0] <= 1:
                             continue
